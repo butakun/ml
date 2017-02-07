303ab1e2908024d73f11cf09c8c4f09e0bd433a4
Hiromasa Kato <hiromasa@iwate-u.ac.jp>
2017-02-07 15:29:40 +0900

    network config params

--- tf/mnist_deep.py
+++ tf/mnist_deep.py
@@ -5,6 +5,12 @@ tf.logging.set_verbosity(tf.logging.INFO)
 
 from tensorflow.examples.tutorials.mnist import input_data
 
+NETWORK_CONFIG = {
+	"Conv1Features":32,
+	"Conv2Features":64,
+	"FC1Features":1024,
+}
+
 def WeightVar(shape):
 
 	return tf.Variable(tf.truncated_normal(shape, stddev = 0.1))
@@ -30,25 +36,25 @@ def Main():
 	x_image = tf.reshape(x, [-1, 28, 28, 1])
 
 	# ReLU(Wx + b)
-	W_conv1 = WeightVar([5, 5, 1, 32])
-	b_conv1 = BiasVar([32])
+	W_conv1 = WeightVar([5, 5, 1, NETWORK_CONFIG["Conv1Features"]])
+	b_conv1 = BiasVar([NETWORK_CONFIG["Conv1Features"]])
 	h_conv1 = tf.nn.relu(Conv2D(x_image, W_conv1) + b_conv1)
 
 	# Pooling
 	h_pool1 = MaxPool(h_conv1, [2, 2])
 
 	# ReLU(Wx + b)
-	W_conv2 = WeightVar([5, 5, 32, 64])
-	b_conv2 = BiasVar([64])
+	W_conv2 = WeightVar([5, 5, NETWORK_CONFIG["Conv1Features"], NETWORK_CONFIG["Conv2Features"]])
+	b_conv2 = BiasVar([NETWORK_CONFIG["Conv2Features"]])
 	h_conv2 = tf.nn.relu(Conv2D(h_pool1, W_conv2) + b_conv2)
 
 	# Pooling
 	h_pool2 = MaxPool(h_conv2, [2, 2])
 
 	# FC1
-	W_fc1 = WeightVar([7 * 7 * 64, 1024])
-	b_fc1 = BiasVar([1024])
-	h_pool2_flat = tf.reshape(h_pool2, [-1, 7 * 7 * 64])
+	W_fc1 = WeightVar([7 * 7 * NETWORK_CONFIG["Conv2Features"], NETWORK_CONFIG["FC1Features"]])
+	b_fc1 = BiasVar([NETWORK_CONFIG["FC1Features"]])
+	h_pool2_flat = tf.reshape(h_pool2, [-1, 7 * 7 * NETWORK_CONFIG["Conv2Features"]])
 	h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1)
 
 	# Dropout
@@ -56,7 +62,7 @@ def Main():
 	h_fc1_drop = tf.nn.dropout(h_fc1, keep_prob)
 
 	# FC2
-	W_fc2 = WeightVar([1024, 10])
+	W_fc2 = WeightVar([NETWORK_CONFIG["FC1Features"], 10])
 	b_fc2 = BiasVar([10])
 	y = tf.matmul(h_fc1_drop, W_fc2) + b_fc2
 
@@ -71,7 +77,7 @@ def Main():
 	init = tf.global_variables_initializer()
 
 	config = tf.ConfigProto(
-		device_count = {"GPU":0}
+#		device_count = {"GPU":0}
 #		log_device_placement = True
 		)
 	sess = tf.Session(config = config)
@@ -95,6 +101,7 @@ def Main():
 	history["TEST"].append((i, accuracy_test))
 
 	params = {}
+	params["NetworkConfig"] = NETWORK_CONFIG
 	params["W_conv1"] = W_conv1.eval(sess)
 	params["b_conv1"] = b_conv1.eval(sess)
 	params["W_conv2"] = W_conv2.eval(sess)
